6c28c6b8c2f1ebf8bf704178090b1b807592c884
Marcos Fabio Pereira <mfpereira@gmail.com>
2017-04-06 07:30:37 -0300

    node communication finished

--- mf-algo/index.js
+++ mf-algo/index.js
@@ -1,7 +1,33 @@
-process.stdin.on('data', (chunk) => {
-  const buf1 = Buffer.from('this is a tést\n');
+const PACKET_SIZE_HEADER = 2;
 
-  if (chunk !== null) {
-    process.stdout.write(buf1);
-  }
+process.stdin.on('data', (chunk) => {
+  var tmpString = Buffer.from(chunk).slice(PACKET_SIZE_HEADER, chunk.length).toString();
+
+  var tmpString = tmpString + " from node";
+
+  const buf1 = Buffer.from(tmpString);
+
+  writeBufferSize(buf1);
+  writeBuffer(buf1);
+
 });
+
+function writeBufferSize(buffer) {
+
+  const bufSizePacket = Buffer.alloc(PACKET_SIZE_HEADER);
+  const size = buffer.length;
+
+  if(PACKET_SIZE_HEADER == 1) {
+    bufSizePacket.write8(size);
+  } else if(PACKET_SIZE_HEADER == 2) {
+    bufSizePacket.writeInt16BE(size);
+  } else if(PACKET_SIZE_HEADER == 4) {
+    bufSizePacket.writeInt32BE(size);
+  }
+
+  process.stdout.write(bufSizePacket);
+}
+
+function writeBuffer(buffer) {
+  process.stdout.write(buffer);
+}
